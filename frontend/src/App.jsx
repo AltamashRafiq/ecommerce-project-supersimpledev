4a2ed9799cc98df1be596f627a75fe40c6936783
Altamash Rafiq <altamashrafiq@Altamashs-MacBook-Pro.local>
2025-06-12 11:06:50 +0500

    split checkout header into its own component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from 'react-router';
-import { HomePage } from './pages/HomePage';
-import { CheckoutPage } from './pages/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
-import './App.css'
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import "./App.css";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
